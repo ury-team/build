bcf17d540b99b922c5b5dcaa2fb59c9e49319ab2
Ma Shihua <mashihua@gmail.com>
2022-01-09 10:57:20 +0800

    change build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git clone git@github.com:ury-team/rent-server.git server
 DOCKER_BUILDKIT=1 docker build --output type=local,dest=./app .
 cp app/billing.jar .
 if [  -f /usr/bin/md5sum  ]; then
-    md5sum -b  billing.jar | awk '//{print $1}' > md5.tx
+    md5sum -b  billing.jar | awk '//{print $1}' > md5.txt
 else
     md5 < billing.jar  > md5.txt
 fi
